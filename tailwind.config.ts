e3c15039013b7719a399f5f8de0c33dee4926d1a
freitashebston <freitashebston@gmail.com>
2026-03-10 12:33:11 +0000

    chore: configurar framework css com processador

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type {config} from 'tailwindcss';
+const config: config= {
+conten:[
+'./src/app/**/*.{js,jsx,ts,tsx}',
+'.src/components/**/*.{js,jsx,ts,tsx}'
+],
+theme:{
+  extend: {}
+},
+plugins:[]
+};
+
+export default config; 
