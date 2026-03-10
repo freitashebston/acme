cb3d49c394414df9a27b03f0927667167d5229ec
freitashebston <freitashebston@gmail.com>
2026-03-10 13:13:51 +0000

    style: adicionar estilizacao temporaria

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 export default function Page ()
 {
-    return 'Sistema em construção.';
+    return (
+        <main className="bg-black h-screen flex justify-center items-center">
+            <h1 className="text-amber-50 font-semibold text-4x1">Sistema em construção.
+            </h1>    
+        </main> 
+    );
 };
 
 
